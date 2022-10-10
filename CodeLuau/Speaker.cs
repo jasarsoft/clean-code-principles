3a341f9e43b0da7d77cf3b3b1d5faf11964eb0b3
Edin Jašarević <edinjasar14@gmail.com>
2022-10-10 19:07:01 +0200

    rename variables

--- CodeLuau/Speaker.cs
+++ CodeLuau/Speaker.cs
@@ -12,7 +12,7 @@ namespace CodeLuau
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string Email { get; set; }
-		public int? Exp { get; set; }
+		public int? YearsExperience { get; set; }
 		public bool HasBlog { get; set; }
 		public string BlogURL { get; set; }
 		public WebBrowser Browser { get; set; }
@@ -32,12 +32,11 @@ namespace CodeLuau
             var error = ValidateData();
             if (error != null) return new RegisterResponse(error);
 
-            //put list of employers in array
-            var emps = new List<string>() { "Pluralsight", "Microsoft", "Google" };
+            var preferredEmployers = new List<string>() { "Pluralsight", "Microsoft", "Google" };
 
-            var good = Exp > 10 || HasBlog || Certifications.Count() > 3 || emps.Contains(Employer);
+            var speakerAppearsQualified = YearsExperience > 10 || HasBlog || Certifications.Count() > 3 || preferredEmployers.Contains(Employer);
 
-            if (!good)
+            if (!speakerAppearsQualified)
             {
                 //need to get just the domain from the email
                 string emailDomain = Email.Split('@').Last();
@@ -45,13 +44,13 @@ namespace CodeLuau
 
                 if (!domains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)))
                 {
-                    good = true;
+                    speakerAppearsQualified = true;
                 }
             }
 
-            if (good)
+            if (speakerAppearsQualified)
             {
-                bool appr = false;
+                bool approved = false;
 
                 if (Sessions.Count() != 0)
                 {
@@ -68,7 +67,7 @@ namespace CodeLuau
                             else
                             {
                                 session.Approved = true;
-                                appr = true;
+                                approved = true;
                             }
                         }
                     }
@@ -78,25 +77,25 @@ namespace CodeLuau
                     return new RegisterResponse(RegisterError.NoSessionsProvided);
                 }
 
-                if (appr)
+                if (approved)
                 {
                     //if we got this far, the speaker is approved
                     //let's go ahead and register him/her now.
                     //First, let's calculate the registration fee. 
                     //More experienced speakers pay a lower fee.
-                    if (Exp <= 1)
+                    if (YearsExperience <= 1)
                     {
                         RegistrationFee = 500;
                     }
-                    else if (Exp >= 2 && Exp <= 3)
+                    else if (YearsExperience >= 2 && YearsExperience <= 3)
                     {
                         RegistrationFee = 250;
                     }
-                    else if (Exp >= 4 && Exp <= 5)
+                    else if (YearsExperience >= 4 && YearsExperience <= 5)
                     {
                         RegistrationFee = 100;
                     }
-                    else if (Exp >= 6 && Exp <= 9)
+                    else if (YearsExperience >= 6 && YearsExperience <= 9)
                     {
                         RegistrationFee = 50;
                     }
@@ -133,7 +132,7 @@ namespace CodeLuau
         private RegisterError? ValidateData()
         {
             if (string.IsNullOrWhiteSpace(FirstName)) return RegisterError.FirstNameRequired;
-            if (!string.IsNullOrWhiteSpace(LastName)) return RegisterError.LastNameRequired;
+            if (string.IsNullOrWhiteSpace(LastName)) return RegisterError.LastNameRequired;
             if (string.IsNullOrWhiteSpace(Email)) return RegisterError.EmailRequired;
 
             return null;
